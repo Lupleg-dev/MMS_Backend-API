985cec4159676fa6a3505524273deaa93deb59f4
Kashish <88932246+kkashish428@users.noreply.github.com>
2023-12-19 22:24:55 +0530

    task done authentication api ready

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRoutes");
-const noteRouter = require("./routes/noteRoutes");
+
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+app.use((req,res,next)=>{
+    console.log("HTTP Method - " + req.method + " , URL - " + req.url);
+    next();
+});
 
 app.use("/users",userRouter);
-app.use("/note",noteRouter);
 
 app.get("/",(req,res)=>{
     res.send("hello");
 });
 
-
-
-
-app.listen(5000, ()=>{
-    console.log("Server started on port no. 5000");
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("Server started on port no." + PORT);
+    });
 })
+.catch((error)=>{
+    console.log(error);
+})
+
+
