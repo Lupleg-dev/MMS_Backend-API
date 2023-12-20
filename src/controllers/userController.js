5c5fda8899558d7f7aa0b71a3fbe727ea652dff8
Kashish <88932246+kkashish428@users.noreply.github.com>
2023-12-20 21:53:02 +0530

    forgot password but coming some error

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,8 @@
 const userModel= require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const randomstring = require("randomstring");
 const SECRET_KEY = "NOTESAPI";
 
 const signup = async (req,res)=>{
@@ -54,4 +56,53 @@ const signin = async (req,res)=>{
     }
 }
 
-module.exports= {signin, signup};
+const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+    try {
+        const existingUser = await userModel.findOne({ email: email });
+        if (!existingUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // Generate a token for password reset
+        const token = jwt.sign({ email: existingUser.email }, SECRET_KEY, { expiresIn: "1h" });
+
+        // Send email with the reset link
+        const resetLink = `http://localhost:3000/users/forgot-password?token=${token}`;
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "kkashish428@gmail.com",
+                pass: "kkashish1",
+            },
+        });
+
+        const mailOptions = {
+            from: "kkashish428@gmail.com",
+            to: email,
+            subject: "Password Reset",
+            text: `Click the following link to reset your password: ${resetLink}`,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
+                return res.status(500).json({ message: "Failed to send reset email" });
+            }
+            console.log("Email sent: " + info.response);
+            res.status(200).json({ message: "Reset email sent successfully" });
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+const resetPassword = async (req, res) => {
+    // Implement the logic to reset the password using the token
+    // Extract token from request and verify it using jwt.verify
+    // If valid, update the user's password in the database
+    // Respond with success or error message accordingly
+};
+
+module.exports= {signin, signup, forgotPassword, resetPassword};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express= require("express");
-const { signup, signin } = require("../controllers/userController");
+const { signup, signin, forgotPassword, resetPassword } = require("../controllers/userController");
 const userRouter = express.Router();
 
 userRouter.post("/signup", signup);
 
 userRouter.post("/signin", signin);
+
+userRouter.post("/forgot-password", forgotPassword);
+
+userRouter.post("/reset-password", resetPassword);
 
 module.exports = userRouter;
