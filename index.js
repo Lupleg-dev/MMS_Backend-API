7e8e838df9cef641c6b558e0ef3e29e5fb4bc52f
Lupleg Dev <152859422+Lupleg-dev@users.noreply.github.com>
2023-12-31 08:36:17 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const userRouter = require("./routes/userRoutes");
+
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use((req,res,next)=>{
+    console.log("HTTP Method - " + req.method + " , URL - " + req.url);
+    next();
+});
+
+app.use("/api/users",userRouter);
+
+app.get("/",(req,res)=>{
+    res.send("hello");
+});
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("Server started on port no." + PORT);
+    });
+})
+.catch((error)=>{
+    console.log(error);
+})
